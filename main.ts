10053d0a7deecd50f3f6d32a931fd8552abfa364
lzty634158 <lzty634158@github.com>
2019-04-10 14:15:19 +0800

    fix ultrosonic

--- main.ts
+++ main.ts
@@ -373,26 +373,28 @@ namespace Tinybit {
 
     }
         
-	//% blockId=Tinybit_Ultrasonic_Car block="ultrasonic return distance(cm)"
+    //% blockId=Tinybit_Ultrasonic_Car block="ultrasonic return distance(cm)"
     //% color="#006400"
     //% weight=87
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function Ultrasonic_Car(): number {
 
-        // send pulse       
-        
-        pins.setPull(DigitalPin.P16, PinPullMode.PullNone);
-        
-        pins.digitalWritePin(DigitalPin.P16, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(DigitalPin.P16, 1);
-        control.waitMicros(15);
-        pins.digitalWritePin(DigitalPin.P16, 0);
-
-        let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
-        let length = Math.floor(d / 40);
-        return  length;
+       let list:Array<number> = [0, 0, 0, 0, 0];
+	for (let i = 0; i < 5; i++)
+	{
+		pins.setPull(DigitalPin.P16, PinPullMode.PullNone);
+		pins.digitalWritePin(DigitalPin.P16, 0);
+		control.waitMicros(2);
+		pins.digitalWritePin(DigitalPin.P16, 1);
+		control.waitMicros(15);
+		pins.digitalWritePin(DigitalPin.P16, 0);
+		let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
+		list[i] = Math.floor(d / 40);
+	}
+	list.sort();
+	let length = (list[1] + list[2] + list[3])/3;
+	return  Math.floor(length);
     }
 
 
